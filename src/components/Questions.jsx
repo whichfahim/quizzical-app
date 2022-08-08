0390b464dc05f97d6c9b2c6efd2e864d71d98b26
Fahim Imtiaz <31901239+whichfahim@users.noreply.github.com>
2022-08-08 13:07:18 +0600

    Fahim | buttons | replaced radio buttons with button elements

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import axios from "axios";
 import parse from "html-react-parser";
 
-export default function Questions() {
+export default function Questions(props) {
   // Make a request for a user with a given ID
   const [questions, setQuestions] = React.useState([]);
+  // const [correctCount, setCorrectCount] = React.useState(0);
 
   React.useEffect(() => {
     axios
       .get("https://opentdb.com/api.php?amount=5&type=multiple")
       .then(function (response) {
         // handle success
-        console.log(response);
         setQuestions(response?.data?.results);
       })
       .catch(function (error) {
@@ -23,21 +23,29 @@ export default function Questions() {
       });
   }, []);
 
+  //once check answers is clicked,
+  // if checked option is correct, apply correct style, else apply wrong style
+
+  const getRandomObject = (array) => {
+    const randomObject = questions[Math.floor(Math.random() * array.length)];
+    return randomObject;
+  };
+
   const questionEl = questions.map((question) => {
     return (
       <div className="card-question">
         <h3>{parse(question.question)}</h3>
-        <div className="options-container">
-          {/* <ul className="donate-now">
+        <div className="form-options">
+          {/* <ul className="ul-options-container">
             <li>
               <input
                 type="radio"
                 id="option-1"
-                name="fav_language"
                 value="option-1"
+                name="radio-group"
                 className="btn-option"
               />
-              <label for="option-1">
+              <label htmlFor="option-1">
                 {parse(question.incorrect_answers[0])}
               </label>
               <br />
@@ -46,11 +54,11 @@ export default function Questions() {
               <input
                 type="radio"
                 id="option-2"
-                name="fav_language"
                 value="option-2"
                 className="btn-option"
+                name="radio-group"
               />
-              <label for="option-2">
+              <label htmlFor="option-2">
                 {parse(question.incorrect_answers[1])}
               </label>
               <br />
@@ -59,12 +67,11 @@ export default function Questions() {
               <input
                 type="radio"
                 id="option-3"
-                name="fav_language"
                 value="option-3"
                 className="btn-option"
-                checked="checked"
+                name="radio-group"
               />
-              <label for="option-3">
+              <label htmlFor="option-3">
                 {parse(question.incorrect_answers[2])}
               </label>
               <br />
@@ -73,25 +80,27 @@ export default function Questions() {
               <input
                 type="radio"
                 id="option-4"
-                name="fav_language"
                 value="option-4"
-                className="btn-option"
+                name="radio-group"
+                className={
+                  props.showAnswers ? "btn-option-correct" : "btn-option"
+                }
               />
-              <label for="option-4">{parse(question.correct_answer)}</label>
+              <label htmlFor="option-4">{parse(question.correct_answer)}</label>
               <br />
             </li>
           </ul> */}
 
-          <button className="btn-option">
+          <button className="btn-option" name="btn-group">
             {parse(question.incorrect_answers[0])}
           </button>
-          <button className="btn-option">
+          <button className="btn-option" name="btn-group">
             {parse(question.correct_answer)}
           </button>
-          <button className="btn-option">
+          <button className="btn-option" name="btn-group">
             {parse(question.incorrect_answers[1])}
           </button>
-          <button className="btn-option">
+          <button className="btn-option" name="btn-group">
             {parse(question.incorrect_answers[2])}
           </button>
         </div>
@@ -100,14 +109,14 @@ export default function Questions() {
     );
   });
 
-  console.log(questionEl);
-
   return (
     <div>
-      <div className="questions-container">
+      <div className="questions-container" key={5}>
         {questionEl}
-        <button className="btn-check">Check Answers</button>
       </div>
+      <button className="btn-check" onClick={props.handleClick}>
+        Check Answers
+      </button>
     </div>
   );
 }
